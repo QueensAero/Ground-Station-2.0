e92a3bf330cf303f6685d6b85d6523fd04819abd
simonsben <simons.ben0@gmail.com>
2018-04-10 20:45:00 -0400

    Added data logging, and heading and height to path/tuple

--- src/main/java/com/QADT/tuple.java
+++ src/main/java/com/QADT/tuple.java
@@ -5,7 +5,15 @@ package com.QADT;
 public class tuple {
 	public double x;
 	public double y;
+	public double h1, h2, head; // Height of point - optional - should become one height at some point...
 	public tuple(double _x, double _y) {
+		x = _x;
+		y = _y;
+	}
+	public tuple(double _x, double _y, double _h1, double _h2, double _head) {
+		h1 = _h1;
+		h2 = _h2;
+		head = _head;
 		x = _x;
 		y = _y;
 	}
@@ -13,4 +21,5 @@ public class tuple {
 	public String toString() {
 		return "(" + Double.toString(x) + ", " + Double.toString(y) + ")";
 	}
+	
 }
